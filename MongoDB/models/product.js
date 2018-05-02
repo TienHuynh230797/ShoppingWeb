eadb7f6d363461bf62a8ffa274fef612311f02c2
Trần Duy Tiên <32634028+tdtien@users.noreply.github.com>
2018-05-02 17:29:11 +0700

    updated Tien

---
+++ MongoDB/models/product.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ProductSchema = new Schema(
+	{
+		product_id: {type: Number, required: true, unique: true},
+		product_name: {type: String, required: true, max: 100},
+		product_company: {type: String, required: true, max: 100},
+		category: {type: String},
+		type: {
+			type: String,
+			enum: ['Women', 'Men', 'Kids', 'Babies']
+		},
+		size_range: {type: String, max: 10},
+		price: {type: Number, required: true},
+		discount: {type: Boolean, required: true},
+		discount_amount: {type: Number}
+		total_quantity: {type: Number}		
+	});
+
+module.exports = mongoose.model('Product', ProductSchema);
